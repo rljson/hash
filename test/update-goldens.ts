d86b498eeb78628fc8a8c5fbd8b23f1a53d67970
Gabriel Gatzsche <gabriel.gatzsche@grace.cloud>
2025-03-08 20:46:40 +0100

    Fix index.ts

--- test/update-goldens.ts
+++ test/update-goldens.ts
@@ -1,8 +1,7 @@
-// Copyright (c) 2024 CARAT Gesellschaft für Organisation
-// und Softwareentwicklung mbH. All Rights Reserved.
+// @license
+// Copyright (c) 2025 Rljson
 //
 // Use of this source code is governed by terms that can be
 // found in the LICENSE file in the root of this package.
 
-/// If this is set to true, the golden files will be updated.
 export const updateGoldens = false;
